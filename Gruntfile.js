71df958b43f13f89c40244e9a028e5624dd682ce
Cameron Dutro <camertron@gmail.com>
2015-08-28 23:37:47 -0500

    Gruntfile should copy images correctly

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function(grunt) {
         files: [{
           expand: true,
           src: ['images/*'],
-          dest: 'dist/images/',
+          dest: 'dist/',
           filter: 'isFile'
         }]
       },
